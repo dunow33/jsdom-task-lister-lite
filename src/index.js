77cc7f7699bb5281a66dffdb789184300e909ce8
dunow <dunow33@hotmail.com>
2021-02-08 14:08:14 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+document.addEventListener("DOMContentLoaded", (event) => { 
+  captureValue(event);
 });
+
+function captureValue(event){
+  document.querySelector("form#create-task-form").addEventListener('submit', function(event) {
+    document.getElementById("tasks").innerHTML += "<li>" + document.getElementById("new-task-description").value + "</li>";
+    document.querySelector("input[type='text']").value = "";
+    event.preventDefault();
+  });
+}
